12d3b55c046c0d78fa5842b912e42a7150e28bfd
swapnaGavinolla <gavinollaswapna@gmail.com>
2023-11-28 07:39:24 +0530

    practicing shell

--- conditions.sh
+++ conditions.sh
@@ -9,8 +9,18 @@
 #     echo "$var_one is not greater than $var_two "
 #     exit 1
 # fi
+user_id = $( id-u )
+if [$user_id -ne 0 ]
+then 
+    echo "get root user access"
+    exit 1
+else
+    echo "go ahead"
+fi
+
+
 yum install nginx -y 
-if [ $? -ne 0]
+if [ $? -ne 0 ]
 then
     echo "nginx is not installed"
     exit 1
