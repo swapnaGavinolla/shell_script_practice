f0e1b44f0354e2c378a6707a9450ae35a4fbcd2e
swapnaGavinolla <gavinollaswapna@gmail.com>
2023-11-28 07:24:57 +0530

    practicing shell

--- variables.sh
+++ variables.sh
@@ -3,3 +3,11 @@ var_one=$1
 var_two=$2
 var_sum=$((var_one+var_two))
 echo "the sum of the given two numbers : $var_sum"
+if ($var_one gt $var_two)
+then  
+    echo "$var_one is greater than $var_two "
+    exit1
+else
+    echo "$var_one is not greater than $var_two "
+    exit1
+fi
