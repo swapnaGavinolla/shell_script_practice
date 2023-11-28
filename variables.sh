2d6b3390421e40d9f7776fc8889d7a4873c3b273
swapnaGavinolla <gavinollaswapna@gmail.com>
2023-11-28 07:21:05 +0530

    practicing shell

--- variables.sh
+++ variables.sh
@@ -1,5 +1,5 @@
 #!/bib/bash
 var_one=$1
 var_two=$2
-var_sum=$((a+$b))
-echo "the sum of the given two numbers : $c"
+var_sum=$((var_one+var_two))
+echo "the sum of the given two numbers : $var_sum"
