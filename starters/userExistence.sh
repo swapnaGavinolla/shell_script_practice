972fb2c7fc10792db1676499c56958c3b47fbadd
swapnaGavinolla <gavinollaswapna@gmail.com>
2023-11-29 23:07:22 +0530

    doing practice

--- starters/userExistence.sh
+++ starters/userExistence.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-id () {
-    id $1 &>> Logfile
-    if [ $? -ne 0 ]
+id(){
+    if [ (id nani) -ne 0]
     then 
-        useradd $1
-        echo "hhh"
-        echo "creating the $1 "
-        
-    # else
-    #     echo "user $1  alrady exists"
-
+        useradd nani
+        echo "added"
+        exit 1
     fi
-} 
-
-id hh
+    echo "fjdgk"
+}
