48ae62d875beca6fd44f6f08eabe7bd4f61296a3
swapnaGavinolla <gavinollaswapna@gmail.com>
2023-11-29 21:34:18 +0530

    doing practice

--- starters/validate_func.sh
+++ starters/validate_func.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-#yum install nginx -y 
-check $? "nginx"
-check(){
-if [ $1 -ne 0 ]
-then
-    echo -e "installation of $2 $R failre $N"
-    exit 1
-else
-    echo -e "installation of $2 $G success $N"
-    exit 1
-fi
+# #yum install nginx -y 
+# Validate $? "nginx"
+# Validate(){
+# if [ $1 -ne 0 ]
+# then
+#     echo -e "installation of $2 $R failre $N"
+#     exit 1
+# else
+#     echo -e "installation of $2 $G success $N"
+#     exit 1
+# fi
+# }
+validate(){
+    
 }
