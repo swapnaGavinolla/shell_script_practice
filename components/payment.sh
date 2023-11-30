77d84b8d27ddf77345a6797f7c91dd070989a8d9
swapnaGavinolla <gavinollaswapna@gmail.com>
2023-11-30 09:06:26 +0530

    practicing

---
+++ components/payment.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+VALIDATE(){
+if [ $1 -ne 0 ]
+then
+    echo -e "installation of $2 $R failre $N"
+    exit 1
+else
+    echo -e "installation of $2 $G success $N"
+fi
+}
+ 
---
+++ components/web.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+VALIDATE(){
+if [ $1 -ne 0 ]
+then
+    echo -e "installation of $2 $R failre $N"
+    exit 1
+else
+    echo -e "installation of $2 $G success $N"
+fi
+}
+
+yum install nginx -y &>> LOGFILE
+VALIDATE $? "nginx"
+
+systemctl enable nginx 
+echo "enabled nginx"
+
+systemctl start nginx
+echo "started nginx"
+
+rm -rf /usr/share/nginx/html/*
+
+
+curl -o /tmp/web.zip https://roboshop-builds.s3.amazonaws.com/web.zip  &>> LOGFILE
+
+
+cd /usr/share/nginx/html
+
+
+unzip /tmp/web.zip &>> LOGFILE
+echo "unzipped"
+
+cp /root/shell_script_practice/starters/roboshop.conf  /etc/nginx/default.d/roboshop.conf 
+echo "copied"
+
+systemctl restart nginx 
+echo "resterted"
+
+ 
