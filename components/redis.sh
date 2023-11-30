b3077344ab2787057344ab0a7da0eca33d4b2cd4
swapnaGavinolla <gavinollaswapna@gmail.com>
2023-11-30 10:53:23 +0530

    practicing

--- components/redis.sh
+++ components/redis.sh
@@ -13,4 +13,20 @@ else
     echo -e "installation of $2 $G success $N"
 fi
 }
- 
+
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>LOGFILE
+VALIDATE $? "installing redis"
+
+yum module enable redis:remi-6.2 -y &>>LOGFILE
+VALIDATE $? "enabling redis"
+
+yum install redis -y &>>LOGFILE
+VALIDATE $? "installing redis"
+
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
+
+systemctl enable redis
+echo "enabled redis"
+
+systemctl start redis
+echo "started redis"
