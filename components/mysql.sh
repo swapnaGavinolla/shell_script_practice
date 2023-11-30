301fb2a122d7fcd170f2df0f304fd51977dfdee7
swapnaGavinolla <gavinollaswapna@gmail.com>
2023-11-30 11:00:23 +0530

    practicing

--- components/mysql.sh
+++ components/mysql.sh
@@ -13,4 +13,20 @@ else
     echo -e "installation of $2 $G success $N"
 fi
 }
+
+yum module disable mysql -y &>>LOGFILE
+VALIDATE $? "disabling mysql"
+
+yum install mysql-community-server -y &>>LOGFILE
+VALIDATE $? "installing mysql community server"
+
+systemctl enable mysqld
+echo "enabled mysql"
+
+systemctl start mysqld
+echo "started mysql"
+
+mysql_secure_installation --set-root-pass RoboShop@1
+
+mysql -uroot -pRoboShop@1
  
