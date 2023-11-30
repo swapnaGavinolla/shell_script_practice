619600ebe8f695386679e6b29fc18a7470fbb2eb
swapnaGavinolla <gavinollaswapna@gmail.com>
2023-11-30 10:11:59 +0530

    practicing

--- components/catalogue.sh
+++ components/catalogue.sh
@@ -13,4 +13,69 @@ else
     echo -e "installation of $2 $G success $N"
 fi
 }
- 
+
+check_user(){
+    id $1 &>> logfile
+    if [ $? -ne 0 ]
+then 
+    useradd $1
+    echo "user $1 added"
+
+else
+    echo " $1 already exits"
+    exit 1
+fi
+}
+
+check_directory_existence (){
+    /$1 &>> LOGFILE
+    if [ $? -ne 0 ]
+    then
+        mkdir $1
+        echo "$1 directory is created"
+        exit 1
+    else
+        echo "$1 directory already exists"
+        exit 1
+    fi
+}
+  
+
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>> LOGFILE
+VALIDATE $? "npm source"
+
+yum install nodejs -y &>> LOGFILE 
+VALIDATE $? "nodejs" 
+
+check_user roboshop
+
+check_directory_existence /app
+
+unzip /tmp/catalogue.zip  &>> LOGFILE
+VALIDATE $? "unzipping"
+
+cd /app
+
+npm install &>> LOGFILE
+VALIDATE $? "npm"
+
+cp /root/shell_script_practice/components/catalogue.service /etc/systemd/system/catalogue.service
+VALIDATE $? "copied"
+
+systemctl daemon-reload
+VALIDATE $? "demon reloading"
+
+systemctl enable catalogue
+VALIDATE $? "enabling"
+
+systemctl start catalogue
+VALIDATE $? "starting"
+
+cp /root/shell_script_practice/components/mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "copied mongo repo"
+
+yum install mongodb-org-shell -y &>> LOGFILE
+VALIDATE $? "mongo-org"
+
+#mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js
+
